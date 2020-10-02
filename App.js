d4b2ce3a730eea060a0d409c2bd54b6966353414
nov0caina <abelmezaenriquez@outlook.com>
2020-10-02 01:25:03 -0600

    Location services added

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location'
+
 
 export default function App() {
+  
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    load()
+  }, [])
+  
+  async function load() {
+    
+    try{
+      let {status} = await Location.requestPermissionsAsync()
+
+      if (status !=  'granted'){
+        setErrorMessage('Access to location is needed in order to run the app')
+        return
+      }
+      const location = await Location.getCurrentPositionAsync()
+
+      const {latitude, longitude } = await location.coords
+      alert(`Latitude: ${latitude}, \nLongitude: ${longitude}`)
+    }
+    catch(error){
+      
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>App under construction!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +41,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    color: 'white',
   },
 });
