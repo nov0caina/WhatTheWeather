6f441daeb1f8b2624024dc5bf4140be745881753
nov0caina <abelmezaenriquez@outlook.com>
2020-10-05 16:47:05 -0600

    API Conexion & WeatherInfo component as well as components folder added

--- App.js
+++ App.js
@@ -1,52 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location'
+import WeatherInfo from './components/WeatherInfo'
 
+const weather_API_KEY = '17d37dcee33e572979460360bde38a67';
+const base_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
 export default function App() {
-  
+
   const [errorMessage, setErrorMessage] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [unitSystem, setUnitsSystem] = useState('metric');
 
   useEffect(() => {
     load()
   }, [])
-  
-  async function load() {
-    
-    try{
-      let {status} = await Location.requestPermissionsAsync()
 
-      if (status !=  'granted'){
+  async function load() {
+
+    try {
+      let { status } = await Location.requestPermissionsAsync()
+
+      if (status != 'granted') {
         setErrorMessage('Access to location is needed in order to run the app')
         return
       }
-      const location = await Location.getCurrentPositionAsync()
+      const location = await Location.getCurrentPositionAsync();
 
-      const {latitude, longitude } = await location.coords
-      alert(`Latitude: ${latitude}, \nLongitude: ${longitude}`)
+      const { latitude, longitude } = await location.coords;
+
+      const weatherUrl = `${base_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitSystem}&appid=${weather_API_KEY}`;
+
+      const response = await fetch(weatherUrl);
+
+      const result = await response.json();
+
+      if (response.ok) {
+        setCurrentWeather(result);
+      }
+      else {
+        setErrorMessage(result.message)
+      }
+
+      /* alert(`Latitude: ${latitude}, \nLongitude: ${longitude}`) */
     }
-    catch(error){
-      
+    catch (error) {
+      setErrorMessage(error.message)
     }
   }
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>App under construction!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  if (currentWeather) {
+    const { 
+      main: { temp },
+    } = currentWeather; 
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={styles.main}>
+        
+        <WeatherInfo currentWeather = {currentWeather} />
+        
+        </View>
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
-    alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    fontSize: 20,
-    color: 'white',
+  main: {
+    justifyContent: 'center',
+    flex: 1,
   },
 });
